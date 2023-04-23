1d3900f2d8834b19aa202614bd9d42526c3c395c
kuyesu <kuyesu22@gmail.com>
2023-04-23 22:13:54 +0300

    bg

---
+++ app/(home)/page.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main className="flex w-full h-full flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
+        <p className=" flex text-orange-500 w-full justify-center  pb-6 pt-8   ">
+          Welcome
+          <span className="font-mono font-bold px-4 ">Hero!</span>
+        </p>
+
+      </div>
+
+      <div className="relative flex place-items-center ">
+
+      </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+
+
+      </div>
+    </main>
+  )
+}
