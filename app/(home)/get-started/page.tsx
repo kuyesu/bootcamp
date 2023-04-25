0476159ea90ad4409e314c5f83f6b302b7452abc
kuyesu <kuyesu22@gmail.com>
2023-04-25 09:10:35 +0300

    redirct to intro

--- app/(home)/get-started/page.tsx
+++ app/(home)/get-started/page.tsx
@@ -2,17 +2,13 @@ import React from 'react'
 import Get from './get'
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth/next";
+import { redirect } from 'next/navigation';
 export default async function page() {
      const session = await getServerSession(authOptions);
     const user = session?.user;
 
     if (!user) {
-        return {
-            redirect: {
-                destination: '/auth/login',
-                permanent: false,
-            },
-        }
+        redirect('/intro')
     }
   return (
       <>
